91ef45946e08c8fa1fd5c8355025d861385bd36a
Yura Sidyakin <yurafikys@gmail.com>
2022-12-15 20:16:27 +0300

    Добавил плагин gulp-rm

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,18 @@
-const {src, dest} = require('gulp')
+const { src, dest, task } = require('gulp')
+const rm = require('gulp-rm');
+
+const files = [
+  'src/styles/one.scss',
+  'src/styles/two.scss'
+]
+
+task('clean', () => {
+  return src('dist/**/*', { read: false })
+    .pipe(rm())
+})
 
 function copy() {
-  return src ('src/styles/main.scss').pipe(dest('dist'))
+  return src(files).pipe(dest('dist'))
 }
 
 exports.copy = copy
