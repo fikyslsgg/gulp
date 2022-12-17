1384c38b761eececf51056e6fc80286b21cdb424
Yura Sidyakin <yurafikys@gmail.com>
2022-12-17 15:30:08 +0300

    Сделал сброку файлов JS

--- gulpfile.js
+++ gulpfile.js
@@ -46,6 +46,14 @@ task("styles", () => {
   .pipe(dest('dist'));
 });
 
+task('scripts', () => {
+  return src('src/scripts/*.js')
+  .pipe(sourcemaps.init())
+  .pipe(concat('main.js'))
+  .pipe(sourcemaps.write())
+  .pipe(dest('dist'));
+})
+
 task('server',  () => {
 
   browserSync.init({
@@ -59,4 +67,4 @@ task('server',  () => {
 
 watch ('./src/styles/**/*.scss', series('styles'))
 watch ('./src/*.html', series('copy:html'))
-task("default", series("clean", "copy:html", "styles", "server"))
+task("default", series("clean", "copy:html", "styles", "scripts", "server"))
